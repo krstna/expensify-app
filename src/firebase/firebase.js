d8a130656332bff39f145f60ca780db35647451a
Kristina Nemkul <creesa.nem@gmail.com>
2018-10-07 13:19:39 +1100

    Add PrivateRoute

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,5 @@
 import * as firebase from 'firebase';
-
+ 
 const config = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -12,8 +12,9 @@ const config = {
 firebase.initializeApp(config);
 
 const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-export {firebase,database as default};
+export {firebase, googleAuthProvider, database as default};
 
 // //child_removed
 // database.ref('expenses').on('child_removed', (snapshot) => {
